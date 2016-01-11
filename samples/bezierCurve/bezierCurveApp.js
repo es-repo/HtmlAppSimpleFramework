115c825fd7fe40890b5ef5a5bd80cbfdf492c6b4
es <eugene.github@gmail.com>
2016-01-11 10:33:48 +0200

    - Somewhat with bezier curve app.

--- samples/bezierCurve/bezierCurveApp.js
+++ samples/bezierCurve/bezierCurveApp.js
@@ -7,13 +7,32 @@ var BezierCurveApp = (function (_super) {
     __extends(BezierCurveApp, _super);
     function BezierCurveApp(graphicOutput, inputControllerHandlers) {
         _super.call(this, graphicOutput, inputControllerHandlers);
+        this.controlPoints = [];
+        this.controlPointSize = 8;
+        this.controlPointColor = new BABYLON.Color4(1, 1, 1, 1);
     }
     BezierCurveApp.prototype.doLogicStep = function () {
+    };
+    BezierCurveApp.prototype.drawFrame = function () {
+        this.drawControlPoints();
+        this.graphicOutput.drawBuffer();
+    };
+    BezierCurveApp.prototype.drawControlPoints = function () {
+        for (var i = 0; i < this.controlPoints.length; i++) {
+            var cp = this.controlPoints[i];
+            var x = cp.x - this.controlPointSize / 2;
+            var y = cp.y - this.controlPointSize / 2;
+            this.renderer2d.drawFilledRectangle(x, y, 0, this.controlPointSize, this.controlPointSize, this.controlPointColor);
+        }
     };
     BezierCurveApp.prototype.handleKeyboardEvent = function (eventArgs) {
         _super.prototype.handleKeyboardEvent.call(this, eventArgs);
     };
     BezierCurveApp.prototype.handleMouseEvent = function (eventArgs) {
+        if (eventArgs.leftButtonClicked && !eventArgs.move) {
+            var cp = new BABYLON.Vector2(eventArgs.x, eventArgs.y);
+            this.controlPoints.push(cp);
+        }
     };
     return BezierCurveApp;
 })(App);
--- samples/bezierCurve/bezierCurveApp.ts
+++ samples/bezierCurve/bezierCurveApp.ts
@@ -1,16 +1,39 @@
 ﻿class BezierCurveApp extends App {    
 
+    private controlPoints: BABYLON.Vector2[] = [];
+    private controlPointSize: number = 8;
+    private controlPointColor: BABYLON.Color4 = new BABYLON.Color4(1, 1, 1, 1);
+
     constructor(graphicOutput: GraphicOutput, inputControllerHandlers: InputDevices) {
-        super(graphicOutput, inputControllerHandlers);
+        super(graphicOutput, inputControllerHandlers);        
     }    
 
     protected doLogicStep() {
-    }    
+    }
+    
+    protected drawFrame() {
+        this.drawControlPoints();        
+        this.graphicOutput.drawBuffer();
+    }   
+    
+    private drawControlPoints()
+    {
+        for (var i = 0; i < this.controlPoints.length; i++){
+            var cp = this.controlPoints[i];
+            var x = cp.x - this.controlPointSize / 2;
+            var y = cp.y - this.controlPointSize / 2;
+            this.renderer2d.drawFilledRectangle(x, y, 0, this.controlPointSize, this.controlPointSize, this.controlPointColor);
+        }        
+    } 
 
     public handleKeyboardEvent(eventArgs: KeyboardEventArgs) {
         super.handleKeyboardEvent(eventArgs);
     }
 
-    public handleMouseEvent(eventArgs: MouseEventArgs) {        
+    public handleMouseEvent(eventArgs: MouseEventArgs) {
+        if (eventArgs.leftButtonClicked && !eventArgs.move) {
+            var cp = new BABYLON.Vector2(eventArgs.x, eventArgs.y);
+            this.controlPoints.push(cp);
+        }        
     }
 }
